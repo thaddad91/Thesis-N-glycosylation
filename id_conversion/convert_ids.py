6847208225d469c7a9046e197e3a54b03d985dd4
thierry <thierry.haddad@wur.nl>
2019-04-30 12:12:43 +0200

    precursor for ID conversion for features

---
+++ id_conversion/convert_ids.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Author: Thierry Haddad
+Description:  Script to retrieve TAIR ID's to be converted to SwissProt ID's.
+              These will be used for sequence-derived characteristics that can
+              be found with SwissProt ID's.
+"""
+# TO-DO: read pickle, single out ID's, convert through SwissProt, append ID's
+#        to the table (make sure to look at AT01G00 and AT01G00.1 etc).
+
+def run_script():
+    pass
+
+if __name__ == "__main__":
+    run_script()
