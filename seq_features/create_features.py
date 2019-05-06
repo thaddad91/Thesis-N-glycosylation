028cddf266247676d2d13f1e4a89e10f6a776932
thierry <thierry.haddad@wur.nl>
2019-05-06 09:12:36 +0200

    Start of proteinko feature tables

---
+++ seq_features/create_features.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+Author:      Thierry Haddad
+Description: Sample 4400 entries (2200 pos/neg) and create sequence-derived
+             features tables.
+"""
+
+from proteinko import Proteinko
+
+prot = Proteinko()
+
+# Add read table func
+
+def create_tables(data = []):
+    """
+    For all the pos and neg sequons, calculate their sequence-derived
+    characteristics like hydropathy, acceptors, donors, iso-electric point
+    and volume. For every feature, a separate table is created. These will
+    afterwards be used in R for ML purposes.
+    """
+    schemes = prt.get_schemas()
+    # Run for every feature
+    for scheme in schemes:
+        table = []  # New table
+        for line in data:
+            seq = data[1]
+            label = data[0]
+            # This step will take long for all entries
+            dist = list(prt.get_dist(seq, scheme))
+            new_line = label + "\t" + "\t".join(dist) + "\n"
+            table.append(new_line)
+        # Write feature table to tab-del file
+        if table != []:
+            with open("table_"+scheme, "w") as f:
+                for line in table:
+                    f.write(line)
+
+def run_script():
+    pass
+
+if __name__ == "__main__":
+    run_script()
