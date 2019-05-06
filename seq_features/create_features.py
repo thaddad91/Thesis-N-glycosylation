0670729e88a8383b7fcdff608d231d41fa5ac3bf
thierry <thierry.haddad@wur.nl>
2019-05-06 14:40:13 +0200

    Added read-table function

--- seq_features/create_features.py
+++ seq_features/create_features.py
@@ -8,22 +8,33 @@ Description: Sample 4400 entries (2200 pos/neg) and create sequence-derived
 
 from proteinko import Proteinko
 
-prot = Proteinko()
+def read_table():
+    """Read tab-del table of pos and neg sequons."""
+    loc = "/mnt/scratch/hadda003/negative_set/pos_neg_table.txt"
+    with open(loc, 'r') as f:
+        data = f.readlines()
+    create_tables(data)
 
-# Add read table func, see negative dir
 
 def create_tables(data = []):
     """
     For all the pos and neg sequons, calculate their sequence-derived
-    characteristics like hydropathy, acceptors, donors, iso-electric point
-    and volume. For every feature, a separate table is created. These will
+    characteristics like:
+    - hydropathy
+    - acceptors
+    - donors
+    - iso-electric point
+    - volume
+    For every feature, a separate table is created. These will
     afterwards be used in R for ML purposes.
     """
+    prt = Proteinko()
     schemes = prt.get_schemas()
     # Run for every feature
     for scheme in schemes:
         table = []  # New table
         for line in data:
+            line = line.split("\t")
             seq = data[3]
             label = data[1]
             # This step will take long for all entries
@@ -35,9 +46,10 @@ def create_tables(data = []):
             with open("table_"+scheme, "w") as f:
                 for line in table:
                     f.write(line)
+        continue  # Debug
 
 def run_script():
-    pass
+    read_tables()
 
 if __name__ == "__main__":
     run_script()
