35cebc040a7925077bd6b9ad55bbce5284bdd404
thierry <thierry.haddad@wur.nl>
2019-05-09 22:03:41 +0200

    Debugged None lines appearing in results

--- seq_features/combine_features.py
+++ seq_features/combine_features.py
@@ -14,8 +14,19 @@ def read_table():
     loc = "/mnt/scratch/hadda003/negative_set/pos_neg_table.txt"
     with open(loc, 'r') as f:
         data = f.readlines()
-    create_tables(data)
 
+    # Reduce negative sequon amount
+    reduced_data = []
+    c = 0
+    for line in data:
+        if "pos" in line:
+            reduced_data.append(line)
+        elif "neg" in line:
+            if c < 12000:
+                reduced_data.append(line)
+        c += 1
+
+    create_tables(reduced_data)
 
 def create_tables(data = []):
     """
@@ -30,45 +41,45 @@ def create_tables(data = []):
     afterwards be used in R for ML purposes.
     """
     # Create 4 workers
-    pool = Pool(processes = 4)
+    pool = Pool(processes = 5)
     # Map calc_dist function to all sequences in the data set
     results = pool.map(calc_dist, data)
     print(len(results))
     with open("table_combined", "w") as f:
         for line in results:
-            f.write(line)
+            if line != None:
+                f.write(line)
     print("Done")
 
 def calc_dist(line):
     """Calculates the distributions per scheme per amino-acid."""
     prt = Proteinko()
-    schemes = prt.get_schemes()
+    schemes = prt.get_schemas()
 
     line = line.split("\t")
     seq = line[3].strip()
-    if not len(seq) == 15:
-        continue
     label = line[1].strip()
     new_line = label
     values = []
-    for scheme in schemes:
-        # This step will take long for all entries
-        dist = list(prt.get_dist(seq, scheme))
-        # Check for only floats
-        for val in dist:
-            try:
-                num = float(val)
-            except:
-                print(seq, val)
-                continue  # Skip due to R issues
-        dist = list(map(str, dist))  # Writeable
-        values.append(dist)
+    if len(seq) == 15:
+        for scheme in schemes:
+            # This step will take long for all entries
+            dist = list(prt.get_dist(seq, scheme))
+            # Check for only floats
+            for val in dist:
+                try:
+                    num = float(val)
+                except:
+                    print(seq, val)
+                    continue  # Skip due to R issues
+            dist = list(map(str, dist))  # Writeable
+            values.extend(dist)
 
-    if len(values) == 6800:
-        new_line += "\t" + "\t".join(values) + "\n"
-        return new_line
-    else:
-        print(len(values)
+        if len(values) == 8500:
+            new_line += "\t" + "\t".join(values) + "\n"
+            return new_line
+        else:
+            print(len(values))
 
 def run_script():
     read_table()
